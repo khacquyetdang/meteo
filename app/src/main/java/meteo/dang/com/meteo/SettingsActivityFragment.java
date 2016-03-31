9aabd63f1eb77263b736b4baffd48b51e3ddebad
khacuyetdang_home <khacuyet.dang.pro@gmail.com>
2016-03-31 15:40:43 +0200

    add reference unit and location Merge branch 'dev' of https://github.com/khacquyetdang/meteo into dev
    # Conflicts:
    #	app/src/main/java/meteo/dang/com/meteo/ForecastFragment.java

--- app/src/main/java/meteo/dang/com/meteo/SettingsActivityFragment.java
+++ app/src/main/java/meteo/dang/com/meteo/SettingsActivityFragment.java
@@ -3,6 +3,8 @@ package meteo.dang.com.meteo;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
 import android.preference.PreferenceFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -29,6 +31,8 @@ public class SettingsActivityFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref_general);
+        EditTextPreference editTextPreference = (EditTextPreference) findPreference(getString(R.string.pref_location_key));
+        ListPreference listPreference = (ListPreference) findPreference(getString(R.string.pref_unit_key));
     }
 
 }
