dc45eec31f688b77366dde4900dec5511b0981fa
khacquyetdang <khacquyet.dang@mail.com>
2016-04-02 09:24:53 +0200

    preference unit and location bind summary ok # Conflicts: #	app/src/main/java/meteo/dang/com/meteo/ForecastFragment.java

--- app/src/main/java/meteo/dang/com/meteo/SettingsActivityFragment.java
+++ app/src/main/java/meteo/dang/com/meteo/SettingsActivityFragment.java
@@ -5,7 +5,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,7 +22,8 @@ import android.view.ViewGroup;
  * Use the {@link SettingsActivityFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SettingsActivityFragment extends PreferenceFragment {
+public class SettingsActivityFragment extends PreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
 
 
     public SettingsActivityFragment() {
@@ -31,8 +34,50 @@ public class SettingsActivityFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref_general);
-        EditTextPreference editTextPreference = (EditTextPreference) findPreference(getString(R.string.pref_location_key));
-        ListPreference listPreference = (ListPreference) findPreference(getString(R.string.pref_unit_key));
+        EditTextPreference locationTextPreference = (EditTextPreference) findPreference(getString(R.string.pref_location_key));
+        ListPreference unitlistPreference = (ListPreference) findPreference(getString(R.string.pref_unit_key));
+
+
+        // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
+        // updated when the preference changes.
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_unit_key)));
+
     }
 
+    /**
+     * Attaches a listener so the summary is always updated with the preference value.
+     * Also fires the listener once, to initialize the summary (so it shows up before the value
+     * is changed.)
+     */
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        // Set the listener to watch for value changes.
+        preference.setOnPreferenceChangeListener(this);
+
+        // Trigger the listener immediately with the preference's
+        // current value.
+        onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        String stringValue = value.toString();
+
+        if (preference instanceof ListPreference) {
+            // For list preferences, look up the correct display value in
+            // the preference's 'entries' list (since they have separate labels/values).
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            // For other preferences, set the summary to the value's simple string representation.
+            preference.setSummary(stringValue);
+        }
+        return true;
+    }
 }
